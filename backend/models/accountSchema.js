ab2f88368dfa74399ab7a287941e9d5c7e86cbb4
shubham kumar <shubhofficial1@gmail.com>
2024-07-29 01:55:13 +0530

    Added Account Schema

---
+++ backend/models/accountSchema.js
@@ -0,0 +1,17 @@
+import mongoose from "mongoose";
+
+const accountSchema = new mongoose.Schema({
+  userId: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: "User",
+    required: true,
+  },
+  balance: {
+    type: Number,
+    required: true,
+  },
+});
+
+const Account = new mongoose.model("Account", accountSchema);
+
+export default Account;
